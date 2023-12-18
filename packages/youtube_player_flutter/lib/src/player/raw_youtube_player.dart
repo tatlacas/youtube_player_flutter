5539d73dde97d5c715517c755fba377b386f73f1
Tatenda Caston Hove <tathove@tatlacas.com>
2023-12-18 14:18:55 +0200

    send player data anyways

--- packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
+++ packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
@@ -292,6 +292,8 @@ class _RawYoutubePlayerState extends State<RawYoutubePlayer>
                 if (playerState == 1) {
                     startSendCurrentTimeInterval();
                     sendVideoData(player);
+                } else {
+                    sendVideoData(player);
                 }
             }
 
